5b2bf742c765ef0e1fb701ceb814800e450f55ed
John Callaway <ovation22@users.noreply.github.com>
2020-09-04 20:15:50 -0400

    Skip Take (#28)
    Nuget Update

--- src/TripleDerby.Core/Specifications/HorseRandomRacerSpecification.cs
+++ src/TripleDerby.Core/Specifications/HorseRandomRacerSpecification.cs
@@ -4,15 +4,17 @@ using TripleDerby.Core.Entities;
 
 namespace TripleDerby.Core.Specifications
 {
-    public class HorseRandomRacerSpecification : Specification<Horse>
+    public sealed class HorseRandomRacerSpecification : Specification<Horse>
     {
         public HorseRandomRacerSpecification()
         {
             Query
                 .Where(x => !x.IsRetired)
                 .OrderBy(x => Guid.NewGuid());
+
             Query.Include(x => x.Statistics);
-            Query.Paginate(0, 10);
+
+            Query.Take(10);
         }
     }
 }
