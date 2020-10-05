7ce862278de4d45e429953a4eafdc8206ea1e690
John Callaway <john@1north.com>
2020-10-04 22:05:54 -0400

    CacheManager tests  - When Cache hit it returns

---
+++ tests/TripleDerby.Infrastructure.Tests/Caching/CacheManagerTests/CacheManagerTestBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+using Moq;
+using TripleDerby.Core.Cache;
+using TripleDerby.Core.Interfaces.Caching;
+using TripleDerby.Infrastructure.Caching;
+
+namespace TripleDerby.Infrastructure.Tests.Caching.CacheManagerTests
+{
+    public class CacheManagerTestBase
+    {
+        protected readonly CacheManager CacheManager;
+        protected readonly Mock<IOptions<CacheConfig>> CacheOptions;
+        protected internal Mock<IDistributedCacheAdapter> CacheAdapter;
+
+        public CacheManagerTestBase()
+        {
+            CacheOptions = new Mock<IOptions<CacheConfig>>();
+            CacheAdapter = new Mock<IDistributedCacheAdapter>();
+
+            CacheOptions.Setup(x => x.Value).Returns(() => new CacheConfig { DefaultExpirationMinutes = 2 });
+
+            CacheManager = new CacheManager(CacheAdapter.Object, CacheOptions.Object);
+        }
+    }
+}
---
+++ tests/TripleDerby.Infrastructure.Tests/Caching/CacheManagerTests/GetOrCreate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Moq;
+using TripleDerby.Core.DTOs;
+using Xunit;
+
+namespace TripleDerby.Infrastructure.Tests.Caching.CacheManagerTests
+{
+    [Trait("Category", "CacheManager")]
+    public class GetOrCreate : CacheManagerTestBase
+    {
+        private readonly string _key;
+
+        public GetOrCreate()
+        {
+            _key = "key";
+        }
+
+        [Fact]
+        public async Task Given_WhenCacheHit_CacheValueReturned()
+        {
+            // Arrange
+            var feedingsResults = new List<FeedingsResult>
+            {
+                new FeedingsResult
+                {
+                    Id = 1,
+                    Name = "Feeding",
+                    Description = "Feeding"
+                }
+            };
+            CacheAdapter.Setup(x => x.GetStringAsync(It.IsAny<string>()))
+                .ReturnsAsync(JsonSerializer.Serialize(feedingsResults));
+
+            // Act 
+            var result = await CacheManager.GetOrCreate(_key, async () => await GetFeedings());
+
+            // Assert
+            Assert.IsAssignableFrom<IEnumerable<FeedingsResult>>(result);
+        }
+
+        private async Task<IEnumerable<FeedingsResult>> GetFeedings()
+        {
+            return await Task.FromResult<IEnumerable<FeedingsResult>>(new List<FeedingsResult>());
+        }
+    }
+}
