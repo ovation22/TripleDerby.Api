2eb07b69a90be71546ea8d54075445908c7d414d
John Callaway <ovation22@users.noreply.github.com>
2020-10-05 21:21:07 -0400

    CacheManager cache miss tests (#43)
    NuGet update

--- tests/TripleDerby.Infrastructure.Tests/Caching/CacheManagerTests/GetOrCreate.cs
+++ tests/TripleDerby.Infrastructure.Tests/Caching/CacheManagerTests/GetOrCreate.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
 using Moq;
 using TripleDerby.Core.DTOs;
 using Xunit;
@@ -10,18 +12,15 @@ namespace TripleDerby.Infrastructure.Tests.Caching.CacheManagerTests
     [Trait("Category", "CacheManager")]
     public class GetOrCreate : CacheManagerTestBase
     {
+        private bool _methodCalled;
         private readonly string _key;
+        private readonly List<FeedingsResult>? _feedingsResults;
 
         public GetOrCreate()
         {
             _key = "key";
-        }
-
-        [Fact]
-        public async Task Given_WhenCacheHit_CacheValueReturned()
-        {
-            // Arrange
-            var feedingsResults = new List<FeedingsResult>
+            _methodCalled = false;
+            _feedingsResults = new List<FeedingsResult>
             {
                 new FeedingsResult
                 {
@@ -30,19 +29,60 @@ namespace TripleDerby.Infrastructure.Tests.Caching.CacheManagerTests
                     Description = "Feeding"
                 }
             };
-            CacheAdapter.Setup(x => x.GetStringAsync(It.IsAny<string>()))
-                .ReturnsAsync(JsonSerializer.Serialize(feedingsResults));
-
-            // Act 
-            var result = await CacheManager.GetOrCreate(_key, async () => await GetFeedings());
-
-            // Assert
-            Assert.IsAssignableFrom<IEnumerable<FeedingsResult>>(result);
         }
 
-        private async Task<IEnumerable<FeedingsResult>> GetFeedings()
+        [Fact]
+        public async Task Given_WhenCacheHit_CacheValueReturned()
         {
-            return await Task.FromResult<IEnumerable<FeedingsResult>>(new List<FeedingsResult>());
+            // Arrange
+            CacheAdapter.Setup(x => x.GetStringAsync(It.IsAny<string>()))
+                .ReturnsAsync(JsonSerializer.Serialize(_feedingsResults));
+
+            // Act 
+            var result = await CacheManager.GetOrCreate(_key, async () => await DefaultMethod());
+
+            // Assert
+            Assert.False(_methodCalled);
+            Assert.IsAssignableFrom<IEnumerable<FeedingsResult>>(result);
+            Assert.Single(result);
+        }
+
+        [Fact]
+        public async Task Given_WhenCacheMiss_ExecutesFunction()
+        {
+            // Arrange
+            CacheAdapter.Setup(x => x.GetStringAsync(It.IsAny<string>()))
+                .ReturnsAsync(string.Empty);
+
+            // Act 
+            var result = await CacheManager.GetOrCreate(_key, async () => await DefaultMethod());
+
+            // Assert
+            Assert.True(_methodCalled);
+            Assert.IsAssignableFrom<IEnumerable<FeedingsResult>>(result);
+            Assert.Single(result);
+        }
+
+        [Fact]
+        public async Task Given_WhenCacheMiss_SetsCache()
+        {
+            // Arrange
+            CacheAdapter.Setup(x => x.GetStringAsync(It.IsAny<string>()))
+                .ReturnsAsync(string.Empty);
+
+            // Act 
+            await CacheManager.GetOrCreate(_key, async () => await DefaultMethod());
+
+            // Assert
+            var value = JsonSerializer.Serialize(_feedingsResults!.ToList());
+            CacheAdapter.Verify(x => x.SetStringAsync(_key, value, It.IsAny<DistributedCacheEntryOptions>()));
+        }
+
+        private async Task<IEnumerable<FeedingsResult>> DefaultMethod()
+        {
+            _methodCalled = true;
+
+            return await Task.FromResult(_feedingsResults.AsEnumerable());
         }
     }
 }
