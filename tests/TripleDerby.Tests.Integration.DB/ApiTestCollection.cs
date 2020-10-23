aa82ccf6a91a3a2d195c42c6b4a81eb9d7e0b68e
John Callaway <ovation22@users.noreply.github.com>
2020-10-22 21:14:33 -0400

    Integration Tests with the DB using Docker (#48)

---
+++ tests/TripleDerby.Tests.Integration.DB/ApiTestCollection.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace TripleDerby.Tests.Integration.DB
+{
+    [CollectionDefinition(CollectionName)]
+    public class ApiTestCollection : ICollectionFixture<ApiTestContext>
+    {
+        public const string CollectionName = "API Test";
+    }
+}
